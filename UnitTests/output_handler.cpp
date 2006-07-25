0724e3933ebc8eb4fa972ec71e394fcd6b50d671
John Millikin <>
2006-07-25 05:03:35 +0000

    Shorter, easier to read message

--- UnitTests/output_handler.cpp
+++ UnitTests/output_handler.cpp
@@ -11,7 +11,7 @@ void DefaultOutputHandler::pass(const Test*) throw() {
 void DefaultOutputHandler::fail(const Test* test, const char* message) throw() {
   /* Output to stderr */
 
-  fprintf(stderr, "Test '%s::%s' failed: %s\n", test->suite_name, test->name,
+  fprintf(stderr, "%s::%s failed: %s\n", test->suite_name, test->name,
     message);
 }
 
