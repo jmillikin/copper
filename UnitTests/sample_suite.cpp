e663ade70d9247a3fac8146c533a1aa8283f4be5
John Millikin <>
2006-08-18 23:26:32 +0000

    Split self-tests into a separate file, so things that would cause errors or failures could be demoed without spurious errors

--- UnitTests/sample_suite.cpp
+++ UnitTests/sample_suite.cpp
@@ -1,141 +1,104 @@
 #include "test.h"
 #include "fixture.h"
 
-// for strdup, in test 'string_equals'
-#include <cstring>
-
 TEST_SUITE(sample_suite)
 
-TEST(assert_failed)
-  // Check that a failed test will result in a pass
-  assert(failed(false));
-
-  // Check that a passing test will result in a fail
-  assert(failed(failed(true)));
-}
-
-TEST(integer_equals)
+TEST(equal)
+  // Equality is performed with the 'equal' function. Equality works
+  // with any two types that can be passed to ==
   assert(equal(2, 2));
-  assert(failed(equal(1, 2)));
-}
 
-TEST(char_string_equals)
-  std::string _var1("test"), _var2("test"), _var3("other");
-  const char
-    *var1 = _var1.c_str(),
-    *var2 = _var2.c_str(),
-    *var3 = _var3.c_str();
-
-  // Confirm that the addresses are not being compared
-  // Can't use unequal for this
-  assert(var1 != var2);
-
-  assert(equal(var1, "test"));
-  assert(equal(var1, var2));
-  assert(failed(equal(var1, var3)));
-}
-
-TEST(mixed_string_equality)
-  std::string var1("test"), _var2("test");
-  const char* var2 = _var2.c_str();
-
-  assert(equal(var1, var2));
+  // Equality also works with C-style strings
+  assert(equal("test", "test"));
 }
 
 TEST(equal_within)
+  // Equal within is used for floating point numbers, where exactness
+  // might not be possible (though it can take any type with >, < and +
+  // operators
   assert(equal_within(2.0, 2.0001, 0.001));
-  assert(failed(equal_within(2.0, 2.01, 0.001)));
 }
 
 TEST(unequal)
+  // Assert that two values are not equal
   assert(unequal(1, 2));
-  assert(failed(unequal(2, 2)));
 }
 
 TEST(is_null)
-  int* var1 = 0, var2;
-  assert(null(var1));
-  assert(failed(null(&var2)));
+  // Used to check that some pointer is NULL
+  int* var = 0;
+  assert(null(var));
 }
 
 TEST(not_null)
-  int* var1 = 0, var2;
-  assert(not_null(&var2));
-  assert(failed(not_null(var1)));
+  // Used to check that some pointer is not NULL
+  int var;
+  assert(not_null(&var));
 }
 
 TEST(greater_than)
+  // Assert some value is > another value
   assert(greater_than(2, 1));
-
-  assert(failed(greater_than(2, 2)));
-  assert(failed(greater_than(2, 3)));
 }
 
 TEST(greater_than_or_equal)
+  // Assert some value is >= another value
   assert(greater_than_or_equal(2, 2));
   assert(greater_than_or_equal(2, 1));
-
-  assert(failed(greater_than_or_equal(1, 2)));
 }
 
 TEST(less_than)
+  // Assert some value is < another value
   assert(less_than(1, 2));
-
-  assert(failed(less_than(2, 2)));
-  assert(failed(less_than(2, 1)));
 }
 
 TEST(less_than_or_equal)
+  // Assert some value is <= another value
   assert(less_than_or_equal(1, 2));
   assert(less_than_or_equal(2, 2));
-
-  assert(failed(less_than_or_equal(2, 1)));
 }
 
-class TestException {
-public:
-  TestException() throw () {}
-  ~TestException() throw() {}
-};
-
-TEST(thrown_exception)
-//  throw TestException();
-}
-
-/*
-TEST(expected_exceptions)
-  bool caught_TestException = false;
-  try {
-    throw TestException();
-  }
-
-  catch (const TestException& e){
-    caught_TestException = true;
-  }
-
-  if (!caught_TestException){
-    fail("Failed to catch TestException");
-  }
-}
-*/
-
+// Fixtures are used for multiple tests that should use the same data.
+// Fixture data is automatically available on the stack, though it must
+// be initialized in set_up(). Dynamically allocated memory or opened
+// files must be closed in tear_down()
 FIXTURE(the_fixture)
 
-void set_up(){
-  fixture_var = 1;
-  set_up_finished = true;
-}
+  void set_up(){
+    fixture_var = 1;
+    set_up_finished = true;
+  }
 
-// If desired, include a "void tear_down(){...
+  // If desired, include a "void tear_down(){...
 
-int fixture_var;
-bool set_up_finished;
+  int fixture_var;
+  bool set_up_finished;
 
 };
 
 FIXTURE_TEST(fixture_test, the_fixture)
   assert(equal(fixture_var, 1));
   assert(set_up_finished);
+}
+
+// Some systems also support catching various programming errors
+
+// NULL-pointer dereference
+TEST(null_deref)
+  int* i = 0;
+  *i = 1;
+}
+
+// Division by zero
+TEST(div_by_zero)
+  int i = 1 / 0;
+}
+
+// Double-freeing (or deleting) some value
+TEST(double_free)
+  int* i = new int;
+  delete i;
+  delete i;
 }
 
 }
