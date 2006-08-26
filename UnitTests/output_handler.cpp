0afdb6a83fd95f7695c7f3020fbb026f7f2f1548
John Millikin <>
2006-08-26 01:09:29 +0000

    Split up output_handler.{cpp,h}

--- UnitTests/output_handler.cpp
+++ UnitTests/output_handler.cpp
@@ -1,57 +1,9 @@
-#include <iostream>
 #include "output_handler.h"
-#include "test.h"
-#include "error_exception.h"
 
 namespace UnitTests {
 
 OutputHandler::OutputHandler() {}
 
 OutputHandler::~OutputHandler() {}
-
-DefaultOutputHandler::DefaultOutputHandler() throw ():
-  OutputHandler(),
-  num_passed(0),
-  num_failed(0),
-  num_errors(0) {
-
-  num_errors = 0;
-}
-
-DefaultOutputHandler::~DefaultOutputHandler() throw (){
-  // Print statistics
-  std::cout
-    << num_passed << " tests passed\n"
-    << num_failed << " tests failed\n"
-    << num_errors << " errors\n";
-}
-
-void DefaultOutputHandler::begin(const Test*) throw () {}
-
-void DefaultOutputHandler::pass(const Test*) throw (){
-  ++num_passed;
-}
-
-void DefaultOutputHandler::fail(const Test* test,
-  const FailureException& failure) throw () {
-
-  ++num_failed;
-
-  std::cerr
-    << "FAILURE in " << test->file_name << ":" << failure.line << ":\n"
-    << test->suite_name << "::" << test->name << ":\n"
-    << "  " << failure.error << "\n\n";
-}
-
-void DefaultOutputHandler::error(const Test* test,
-  const ErrorException& error) throw () {
-
-  ++num_errors;
-
-  std::cerr
-    << "ERROR in " << test->file_name << ":\n"
-    << test->suite_name << "::" << test->name << ":\n"
-    << "  " << error.message << "\n\n";
-}
 
 } /* namespace */
--- UnitTests/output_handler.h
+++ UnitTests/output_handler.h
@@ -52,58 +52,6 @@ public:
     throw () = 0;
 };
 
-/**
-  This is a default implementation of an OutputHandler. It will output failures
-  to the standard error stream
-*/
-class DefaultOutputHandler : public OutputHandler {
-public:
-  /** Default constructor */
-  DefaultOutputHandler() throw ();
-
-  /** Default destructor */
-  ~DefaultOutputHandler() throw ();
-
-  /** Does nothing */
-  void begin(const Test* test) throw ();
-
-  /**
-    Outputs nothing, but keeps track of how many tests passed
-
-    @param test The test that passed
-  */
-  void pass(const Test* test) throw ();
-
-  /**
-    Outputs failures to the standard error stream. Also keeps track of how
-    many tests failed
-
-    @param test The test that failed
-    @param failure The failure that was thrown
-  */
-  void fail(const Test* test, const FailureException& failure)
-    throw ();
-
-  /**
-    Outputs errors to the standard error stream. Also keeps track of how
-    many tests errored out
-
-    @param test The test that failed
-    @param error The error that was thrown
-  */
-  void error(const Test* test, const ErrorException& error) throw ();
-
-protected:
-  /** The number of tests that have passed */
-  unsigned int num_passed;
-
-  /** The number of failed tests */
-  unsigned int num_failed;
-
-  /** The number of tests that had errors */
-  unsigned int num_errors;
-};
-
 } /* namespace */
 
 #endif /* OUTPUT_HANDLER_H */
