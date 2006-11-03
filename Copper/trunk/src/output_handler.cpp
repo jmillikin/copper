aa24790fcfc713314817aca56640f5031c26b9a1
John Millikin <>
2006-11-03 02:06:34 +0000

    Removed exception handling logic from output_handler.cpp

--- Copper/trunk/src/output_handler.cpp
+++ Copper/trunk/src/output_handler.cpp
@@ -6,25 +6,13 @@
 #include <algorithm>
 #include <copper/output_handler.hpp>
 #include <copper/protector.hpp>
-#include "protectors/exception_protector.hpp"
 #include <copper/assertion.hpp>
 #include "export.hpp"
 
 namespace Copper {
 
-OutputHandler::OutputHandler(int& argc, char**& argv) {
-  // Allow exception catching to be toggled on or off at runtime
-  bool catch_exceptions = true;
-  for (int ii = 1; ii < argc; ii++) {
-    if (strcmp(argv[ii], "--no-exceptions") == 0) {
-      catch_exceptions = false;
-    }   
-  }
-
-  if (catch_exceptions) {
-    static ExceptionProtector exception_protector;
-    Protector::add(&exception_protector);
-  }
+OutputHandler::OutputHandler(int&, char**&) {
+  // For now, do nothing
 }
 
 OutputHandler::~OutputHandler() {}
