56fef6db776d0917fd7f4c48f2b9833dba012aa3
John Millikin <>
2006-11-10 19:39:57 +0000

    Disable warning 4996 in Visual studio

--- Copper/trunk/include/copper/compat.hpp
+++ Copper/trunk/include/copper/compat.hpp
@@ -1,4 +1,8 @@
-// Disable warnings about throw specifications in Visual Studio
+// For visual studio
 #ifdef _MSC_VER
+// Disable warnings about throw specifications
 #pragma warning(disable: 4290)
+
+// Disable warnings about ANSI C functions being deprecated
+#pragma warning(disable: 4996)
 #endif
