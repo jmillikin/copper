95f0ace4f4e090aeb5c01584d9d3cfad5961fba4
John Millikin <jmillikin@gmail.com>
2007-03-07 12:00:24 -0800

    Added more tests for the argument parser

--- tests/argument_parser_tests.cpp
+++ tests/argument_parser_tests.cpp
@@ -5,14 +5,65 @@
 
 #include <copper/output_handler.hpp>
 #include <copper.hpp>
+#include <cstdio>
+
+typedef Copper::List<Copper::Test> TestList;
+TestList parse_test_args(int argc, char** argv) {
+  return Copper::OutputHandler::parse_test_args(argc, argv);
+}
 
 // Test the argument parser
 TEST_SUITE(argument_parser_tests) {
 
 TEST(no_arguments) {
-  Copper::List<Copper::Test> tests;
   int argc = 0;
   char* argv[] = {};
+  Copper::List<Copper::Test> tests = parse_test_args(argc, argv);
+
+  assert(equal(Copper::Test::all().size(), tests.size()));
+}
+
+TEST(entire_suite) {
+  int argc = 1;
+  char* argv[] = {"argument_parser_tests"};
+  Copper::List<Copper::Test> tests = parse_test_args(argc, argv);
+
+  assert(equal(current_suite.get_tests().size(), tests.size()));
+}
+
+TEST(single_test) {
+  int argc = 1;
+  char* argv[] = {"argument_parser_tests.single_test"};
+  Copper::List<Copper::Test> tests = parse_test_args(argc, argv);
+
+  assert(equal(1, tests.size()));
+}
+
+TEST(multiple_tests) {
+  int argc = 2;
+  char* argv[] = {
+    "argument_parser_tests.single_test",
+    "argument_parser_tests.multiple_tests",
+  };
+  Copper::List<Copper::Test> tests = parse_test_args(argc, argv);
+
+  assert(equal(2, tests.size()));
+}
+
+TEST(invalid_suite) {
+  int argc = 1;
+  char* argv[] = {"nonexistant_suite"};
+  Copper::List<Copper::Test> tests = parse_test_args(argc, argv);
+
+  assert(equal(0, tests.size()));
+}
+
+TEST(invalid_test) {
+  int argc = 1;
+  char* argv[] = {"argument_parser_tests.nonexistant_test"};
+  Copper::List<Copper::Test> tests = parse_test_args(argc, argv);
+
+  assert(equal(0, tests.size()));
 }
 
 }
