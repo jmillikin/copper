21563c13c453847b664ec7b07eb72cffa104ab35
John Millikin <jmillikin@gmail.com>
2007-04-13 21:50:10 -0700

    Updated test files to new code style

--- tests/format_tests.cpp
+++ tests/format_tests.cpp
@@ -1,4 +1,4 @@
-/* format_tests.cpp - Test behaviour of the format() function
+/* format_tests.cpp - Test behaviour of the format () function
  * Copyright (C) 2006 John Millikin
  * For conditions of distribution and use, see COPYING
  */
@@ -23,99 +23,113 @@ const signed long MY_LONG_MIN = -MY_LONG_MAX - 1;
 
 const unsigned long MY_ULONG_MAX = 4294967295UL;
 
-TEST_SUITE(format_tests) {
+TEST_SUITE(format_tests)
+{
+	TEST (bool_test)
+	{
+		ASSERT (equal ("true", format (true)));
+		ASSERT (equal ("false", format (false)));
+	}
 
-TEST(bool_test) {
-  ASSERT(equal("true", format(true)));
-  ASSERT(equal("false", format(false)));
-}
+	TEST (char_test)
+	{
+		/* Bad characters */
+		for (char ii = '\0'; ii < ' '; ii++)
+		{
+			ASSERT (equal ("", format (ii)));
+		}
 
-TEST(char_test) {
-  /* Bad characters */
-  for (char ii = '\0'; ii < ' '; ii++) {
-    ASSERT(equal("", format(ii)));
-  }
-  /* 0x7F = DEL */
-  ASSERT(equal("", format('\x7F')));
+		/* 0x7F = DEL */
+		ASSERT (equal ("", format ('\x7F')));
 
-  char buffer[2];
-  buffer[1] = '\0';
+		char buffer[2];
+		buffer[1] = '\0';
 
-  for (buffer[0] = ' '; buffer[0] < '~'; buffer[0]++) {
-    ASSERT(equal(buffer, format(buffer[0])));
-  }
-}
+		for (buffer[0] = ' '; buffer[0] < '~'; buffer[0]++)
+		{
+			ASSERT (equal (buffer, format (buffer[0])));
+		}
+	}
 
-TEST(signed_short_test) {
-  signed short neg_one = -1, pos_one = 1, zero = 0;
-  ASSERT(equal("-1", format(neg_one)));
-  ASSERT(equal("1", format(pos_one)));
-  ASSERT(equal("0", format(zero)));
+	TEST (signed_short_test)
+	{
+		signed short neg_one = -1, pos_one = 1, zero = 0;
+		ASSERT (equal ("-1", format (neg_one)));
+		ASSERT (equal ("1", format (pos_one)));
+		ASSERT (equal ("0", format (zero)));
 
-  /* Check limits */
-  ASSERT(equal("-32768", format(MY_SHRT_MIN)));
-  ASSERT(equal("32767", format(MY_SHRT_MAX)));
-}
+		/* Check limits */
+		ASSERT (equal ("-32768", format (MY_SHRT_MIN)));
+		ASSERT (equal ("32767", format (MY_SHRT_MAX)));
+	}
 
-TEST(unsigned_short_test) {
-  unsigned short pos_one = 1, zero = 0;
-  ASSERT(equal("1", format(pos_one)));
-  ASSERT(equal("0", format(zero)));
+	TEST (unsigned_short_test)
+	{
+		unsigned short pos_one = 1, zero = 0;
+		ASSERT (equal ("1", format (pos_one)));
+		ASSERT (equal ("0", format (zero)));
 
-  /* Check limits */
-  ASSERT(equal("65535", format(MY_USHRT_MAX)));
-}
+		/* Check limits */
+		ASSERT (equal ("65535", format (MY_USHRT_MAX)));
+	}
 
-TEST(signed_int_test) {
-  signed int neg_one = -1, pos_one = 1, zero = 0;
-  ASSERT(equal("-1", format(neg_one)));
-  ASSERT(equal("1", format(pos_one)));
-  ASSERT(equal("0", format(zero)));
+	TEST (signed_int_test)
+	{
+		signed int neg_one = -1, pos_one = 1, zero = 0;
+		ASSERT (equal ("-1", format (neg_one)));
+		ASSERT (equal ("1", format (pos_one)));
+		ASSERT (equal ("0", format (zero)));
 
-  /* Check limits */
-  ASSERT(equal("-2147483648", format(MY_INT_MIN)));
-  ASSERT(equal("2147483647", format(MY_INT_MAX)));
-}
+		/* Check limits */
+		ASSERT (equal ("-2147483648", format (MY_INT_MIN)));
+		ASSERT (equal ("2147483647", format (MY_INT_MAX)));
+	}
 
-TEST(unsigned_int_test) {
-  unsigned int pos_one = 1, zero = 0;
-  ASSERT(equal("1", format(pos_one)));
-  ASSERT(equal("0", format(zero)));
+	TEST (unsigned_int_test)
+	{
+		unsigned int pos_one = 1, zero = 0;
+		ASSERT (equal ("1", format (pos_one)));
+		ASSERT (equal ("0", format (zero)));
 
-  /* Check limits */
-  ASSERT(equal("4294967295", format(MY_UINT_MAX)));
-}
+		/* Check limits */
+		ASSERT (equal ("4294967295", format (MY_UINT_MAX)));
+	}
 
-TEST(signed_long_test) {
-  signed long neg_one = -1, pos_one = 1, zero = 0;
-  ASSERT(equal("-1", format(neg_one)));
-  ASSERT(equal("1", format(pos_one)));
-  ASSERT(equal("0", format(zero)));
+	TEST (signed_long_test)
+	{
+		signed long neg_one = -1, pos_one = 1, zero = 0;
+		ASSERT (equal ("-1", format (neg_one)));
+		ASSERT (equal ("1", format (pos_one)));
+		ASSERT (equal ("0", format (zero)));
 
-  /* Check limits */
-  ASSERT(equal("-2147483648", format(MY_LONG_MIN)));
-  ASSERT(equal("2147483647", format(MY_LONG_MAX)));
-}
+		/* Check limits */
+		ASSERT (equal ("-2147483648", format (MY_LONG_MIN)));
+		ASSERT (equal ("2147483647", format (MY_LONG_MAX)));
+	}
 
-TEST(unsigned_long_test) {
-  unsigned long pos_one = 1, zero = 0;
-  ASSERT(equal("1", format(pos_one)));
-  ASSERT(equal("0", format(zero)));
+	TEST (unsigned_long_test)
+	{
+		unsigned long pos_one = 1, zero = 0;
+		ASSERT (equal ("1", format (pos_one)));
+		ASSERT (equal ("0", format (zero)));
 
-  /* Check limits */
-  ASSERT(equal("4294967295", format(MY_ULONG_MAX)));
-}
+		/* Check limits */
+		ASSERT (equal ("4294967295", format (MY_ULONG_MAX)));
+	}
 
-TEST(float_test) {
-  ASSERT(equal("1.2345", format(1.2345f)));
-}
+	TEST (float_test)
+	{
+		ASSERT (equal ("1.2345", format (1.2345f)));
+	}
 
-TEST(double_test) {
-  ASSERT(equal("1.2345", format(1.2345)));
-}
+	TEST (double_test)
+	{
+		ASSERT (equal ("1.2345", format (1.2345)));
+	}
 
-TEST(Copper_String_test) {
-  ASSERT(equal("some_string", format(Copper::String("some_string"))));
-}
-
+	TEST (Copper_String_test)
+	{
+		ASSERT (equal ("some_string",
+		               format (Copper::String("some_string"))));
+	}
 }
