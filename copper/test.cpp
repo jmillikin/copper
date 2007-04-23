06f13821dba0325cbe638d1b1395aec1d88ed6d0
John Millikin <jmillikin@gmail.com>
2007-04-22 20:37:21 -0700

    Improved the docs for Test.

--- copper/test.cpp
+++ copper/test.cpp
@@ -15,28 +15,30 @@ namespace Copper
 	}
 
 	/** @class Test
+	 * @brief Abstract base class for tests.
+	 * 
 	 * Does nothing interesting by itself, but is subclassed to provide
 	 * runnable test classes.
 	 */
 
 	/** @var Test::name
-	 * The name of this test.
+	 * @brief The name of this test.
 	 */
 
 	/** @var Test::suite
-	 * The suite this test is part of.
+	 * @brief The suite this test is part of.
 	 */
 
 	/** @var Test::file_name
-	 * The file this test's implementation is in.
+	 * @brief The file this test's implementation is in.
 	 */
 
 	/** @var Test::line
-	 * The line this test was defined on.
+	 * @brief The line this test was defined on.
 	 */
 
 	/**
-	 * Constructs a new test.
+	 * @brief Constructs a new test.
 	 * 
 	 * @param name The name of this test. This must be static data.
 	 * @param suite The suite this test is part of. This must be
@@ -58,12 +60,18 @@ namespace Copper
 		all_tests ().append (this);
 	}
 
-	/** Default destructor */
+	/**
+	 * @brief Default destructor
+	 */
 	Test::~Test ()
 	{
 	}
 
-	/** Get a list of all tests */
+	/**
+	 * @brief Get a list of all tests
+	 * 
+	 * @return a list of all tests.
+	 */
 	const List<Test> &
 	Test::all ()
 	{
@@ -130,16 +138,19 @@ namespace Copper
 		return all_tests ().filter (suite_matcher, &info);
 	}
 
-	/** Default, does nothing */
+	/**
+	 * @brief Does nothing
+	 */
 	void
 	Test::set_up ()
 	{
 	}
 
-	/** Default, does nothing */
+	/**
+	 * @brief Does nothing
+	 */
 	void
 	Test::tear_down ()
 	{
 	}
-
 }
