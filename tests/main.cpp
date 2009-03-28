8c3fdd06a200076a7a5229ec0195d5886fc71b72
John Millikin <jmillikin@gmail.com>
2009-03-28 14:40:26 -0700

    Simplified tests/main.cpp.

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,23 +3,12 @@
  * For conditions of distribution and use, see COPYING
  */
 
-#include <cstdlib>
 #include <copper/DefaultOutputHandler.hpp>
-
-Copper::OutputHandler *output;
-
-void
-do_exit ()
-{
-	delete output;
-}
 
 int
 main (int argc, char** argv)
 {
-	atexit (do_exit);
-
 	// Output to the console
-	output = new Copper::DefaultOutputHandler (argc, argv);
-	return output->run ();
+	Copper::DefaultOutputHandler output (argc, argv);
+	return output.run ();
 }
